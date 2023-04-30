783e06836217aa494cc420092b757bead0178fb6
Roy543 <sahilsas88@gmail.com>
2023-04-30 14:19:00 +0200

    Dropdown added

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -6,11 +6,11 @@
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="userAge"/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -54,14 +54,18 @@
 
 <script>
 export default {
-  methods: {
-    data(){
+  data(){
       return{
-        userName: ''
+        userName: '',
+        userAge: null,
+        referrer: 'wom'
       };
     },
+  methods: {
     submitForm(){
       this.userName = '';
+      this.userAge = null;
+      this.referrer = 'wom';
     }
   }
 }
